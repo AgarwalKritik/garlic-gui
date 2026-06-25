7e7a5b3197b5ca79a4a0fcfffe4f1848a2604262
Kritik Agarwal <kritikagarwal@ymail.com>
2026-06-25 18:02:59 +0530

    style(garlic_wrapper): inject structural block dividers and copyright headers

--- src/garlic/garlic_wrapper.h
+++ src/garlic/garlic_wrapper.h
@@ -1,21 +1,24 @@
-//  Copyright 2026 Kritik Agarwal
+// ==============================================================================
+//               Copyright 2026 Kritik Agarwal
 //
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//          http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
+// ==============================================================================
 //
-// Garlic Wrapper - Interface between C++ GUI and Garlic C decompiler
-// This file provides C interface functions that can be called from C++
-
+// File: garlic_wrapper.h
+// Description: Header for the C garlic_wrapper.
+//
+// ==============================================================================
 #ifndef GARLIC_WRAPPER_H
 #define GARLIC_WRAPPER_H
 
@@ -24,22 +27,30 @@ extern "C"
 {
 #endif
 
-    // Main decompilation function
+    // ==============================================================================
+    // Main Decompilation API
+    // ==============================================================================
     // Returns 1 on success, 0 on failure
     int garlic_decompile_file(const char *input_path, const char *output_path, int thread_num);
 
-    // Progress callback registration
+    // ==============================================================================
+    // Progress Tracking
+    // ==============================================================================
     // Callback will be called with progress values from 0 to 100
     void garlic_set_progress_callback(void (*callback)(int progress));
 
-    // File validation functions
+    // ==============================================================================
+    // File Validation API
+    // ==============================================================================
     int garlic_is_valid_file(const char *file_path);
     int garlic_is_valid_apk(const char *file_path);
     int garlic_is_valid_dex(const char *file_path);
     int garlic_is_valid_jar(const char *file_path);
     int garlic_is_valid_class(const char *file_path);
 
-    // Utility functions
+    // ==============================================================================
+    // Utility API
+    // ==============================================================================
     const char *garlic_get_version();
     const char *garlic_get_file_type_string(const char *file_path);
     int garlic_get_current_progress();

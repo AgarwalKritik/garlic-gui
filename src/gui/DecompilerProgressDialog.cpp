f4e3ff67f9dc414447a1ebc8a37fd54b57937d21
Kritik Agarwal <kritikagarwal@ymail.com>
2026-06-25 18:03:01 +0530

    style(DecompilerProgressDialog): inject structural block dividers and copyright headers

--- src/gui/DecompilerProgressDialog.cpp
+++ src/gui/DecompilerProgressDialog.cpp
@@ -1,20 +1,29 @@
-//  Copyright 2026 Kritik Agarwal
+// ==============================================================================
+//               Copyright 2026 Kritik Agarwal
 //
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//          http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
-
+// ==============================================================================
+//
+// File: DecompilerProgressDialog.cpp
+// Description: Dialog UI for displaying extraction and decompilation progress.
+//
+// ==============================================================================
 #include "DecompilerProgressDialog.h"
 
+// ==============================================================================
+// Method: DecompilerProgressDialog::DecompilerProgressDialog
+// ==============================================================================
 DecompilerProgressDialog::DecompilerProgressDialog(QWidget *parent)
     : QDialog(parent)
 {
@@ -24,6 +33,9 @@ DecompilerProgressDialog::DecompilerProgressDialog(QWidget *parent)
     setFixedSize(400, 150);
 }
 
+// ==============================================================================
+// Method: DecompilerProgressDialog::setupUI
+// ==============================================================================
 void DecompilerProgressDialog::setupUI()
 {
     m_mainLayout = new QVBoxLayout(this);
@@ -63,16 +75,25 @@ void DecompilerProgressDialog::setupUI()
     setLayout(m_mainLayout);
 }
 
+// ==============================================================================
+// Method: DecompilerProgressDialog::updateProgress
+// ==============================================================================
 void DecompilerProgressDialog::updateProgress(int progress)
 {
     m_progressBar->setValue(progress);
 }
 
+// ==============================================================================
+// Method: DecompilerProgressDialog::setStatusText
+// ==============================================================================
 void DecompilerProgressDialog::setStatusText(const QString &text)
 {
     m_statusLabel->setText(text);
 }
 
+// ==============================================================================
+// Method: DecompilerProgressDialog::onCancelClicked
+// ==============================================================================
 void DecompilerProgressDialog::onCancelClicked()
 {
     emit cancelled();

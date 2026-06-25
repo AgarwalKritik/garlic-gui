5533dbd71ab47f4e562fe1909df82169de4f41ba
Kritik Agarwal <kritikagarwal@ymail.com>
2026-06-25 18:03:04 +0530

    style(WelcomeWidget): inject structural block dividers and copyright headers

--- src/gui/WelcomeWidget.h
+++ src/gui/WelcomeWidget.h
@@ -1,18 +1,24 @@
-//  Copyright 2026 Kritik Agarwal
+// ==============================================================================
+//               Copyright 2026 Kritik Agarwal
 //
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//          http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
-
+// ==============================================================================
+//
+// File: WelcomeWidget.h
+// Description: Header for WelcomeWidget.
+//
+// ==============================================================================
 #ifndef WELCOMEWIDGET_H
 #define WELCOMEWIDGET_H
 
@@ -27,13 +33,22 @@ class WelcomeWidget : public QWidget
     Q_OBJECT
 
 public:
+    // ==============================================================================
+    // Public Interface
+    // ==============================================================================
     explicit WelcomeWidget(QWidget *parent = nullptr);
 
 signals:
+    // ==============================================================================
+    // Signals
+    // ==============================================================================
     void openFileRequested();
     void openProjectRequested();
 
 private:
+    // ==============================================================================
+    // Private Members & Methods
+    // ==============================================================================
     void setupUI();
 };
 
